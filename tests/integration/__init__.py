5efd810b727b75754c6f831c9719f0e9c5a69638
Sergei Ulitskii <sergeiulitskii@kukuku.local>
2025-05-23 10:31:06 -0700

    feat: Streamline testing infrastructure with intelligent Docker cleanup and fix container naming

--- tests/integration/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Integration Tests for TelegramGroupie
-
-This package contains integration tests that test multiple components working
-together using mock services for fast, reliable testing.
-"""
